cbd4a3e43cfaf905a247379492c59a3833eabd65
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-05-26 15:05:11 +0100

    clean initial code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,26 +2,5 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'AwidCar'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('AwidCar');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AwidCar app is running!');
-  });
+    
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GroupOperationHomeComponent } from './components/group-operation-home/group-operation-home.component';
+import { GroupOperationUniqueComponent } from './components/group-operation-unique/group-operation-unique.component';
+import { PersonalInfoComponent } from './components/personal-info/personal-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupOperationHomeComponent,
+    GroupOperationUniqueComponent,
+    PersonalInfoComponent
   ],
   imports: [
     BrowserModule
